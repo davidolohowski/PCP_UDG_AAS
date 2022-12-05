423b02ee4da7f4ed869400c0bd090ffb0043cbac
David Li <30305423+davidolohowski@users.noreply.github.com>
2022-12-05 11:56:43 -0500

    Tidy up

--- src/v10acs_da.R
+++ src/v10acs_da.R
@@ -10,12 +10,14 @@ library(tikzDevice)
 library(expint)
 library(gplots)
 
+# Read in data and functions
 options(warn = -1)
 source('src/help_functions.R')
 v_acs <- read_csv('data/v10acs_GC.csv')
 col <- readRDS('data/v10_acs_col.RDS')
 pts <- v_acs[,3:4]/4300
 
+#fixed parameters for normal elliptical galaxies
 gal_fix <- data.frame(x = c(3278)/4300, y = c(4073)/4300,
                       theta = c(0), e = c(1/1.4))
 
@@ -73,10 +75,12 @@ prob_col <- t(prob)
 prob_ncol <- prob_ncol[-c(1:10000),]
 prob_col <- prob_col[-c(1:10000),]
 
+# R hat statistics for Model 1 and Model 2
 PSRF_ncol <- stable.GR(prob_ncol, multivariate = F)
 PSRF_col <- stable.GR(prob_col, multivariate = F)
 
-# posterior density plots
+# posterior density plots for fixed dimensional parameters
+# Model 1
 ncol_mod <- as.data.frame(exp(res[[2]][-c(1:10000),]))
 prior_df_ncol <- data.frame(value = c(seq(min(ncol_mod$b0), max(ncol_mod$b0), length= 100), 
                                       seq(min(ncol_mod$lc), max(ncol_mod$lc), length= 100),
@@ -91,6 +95,7 @@ prior_df_ncol <- data.frame(value = c(seq(min(ncol_mod$b0), max(ncol_mod$b0), le
                             key = c(rep('b0', 100), rep('lc', 100), rep('n1', 100), rep('N1', 100), 
                                     rep('R1', 100)))
 
+# Model 2
 col_mod <- as.data.frame(exp(res_col[[2]][-c(1:10000),]))
 prior_df_col <- data.frame(value = c(seq(min(col_mod$b0), max(col_mod$b0), length= 100), 
                                      seq(min(col_mod$lc), max(col_mod$lc), length= 100),
@@ -144,6 +149,7 @@ X_c <- as.data.frame(X_c)
 N_nc <- 1*(unlist(lapply(ncol_dat, nrow)) == 0)
 N_c <- 1*(unlist(lapply(col_dat, nrow)) == 0)
 
+# P(Nc = 0)
 sum(N_nc)/90001
 sum(N_c)/90001
 
@@ -156,9 +162,11 @@ pu_c <- numeric(100)
 hist_res_nc <- hist2d(X_nc$x, X_nc$y, nbins = c(7, 7))
 hist_res_c <- hist2d(X_c$x, X_c$y, nbins = c(7, 7))
 
+# max_iP(UDG in omega_i)
 max(hist_res_nc$counts)/nrow(X_nc)*(1 - sum(N_nc)/90001)
 max(hist_res_c$counts)/nrow(X_c)*(1 - sum(N_c)/90001)
 
+# Block bootstrap for CI
 set.seed(109857)
 for (i in 1:100) {
   block_idx <- sample(1:720, 720, replace = T)
@@ -179,6 +187,7 @@ for (i in 1:100) {
   pu_c[i] <- max(hist_res_c_j$counts)/nrow(X_c_j)*(1 - sum(N_c_j)/90001)
 }
 
+# Posterior densities for parameters of UDGs
 U <- data.frame(x = 3609, y = 631)/4300
 
 df_U_ncol <- do.call(rbind, res[[1]][-c(1:10001)]) %>%
@@ -189,6 +198,7 @@ df_U_col <- do.call(rbind, res_col[[1]][-c(1:10001)]) %>%
   filter((x - U$x)^2 + (y - U$y)^2 <= 0.01^2)%>%
   dplyr::select(N, R, n, sigma)
 
+# Model 1
 prior_df_ncol_U <- data.frame(value = c(seq(min(df_U_ncol$N), max(df_U_ncol$N), length= 100), 
                                         seq(min(df_U_ncol$R), max(df_U_ncol$R), length= 100),
                                         seq(min(df_U_ncol$n), max(df_U_ncol$n), length= 100)),
@@ -208,6 +218,7 @@ ggplot(gather(df_U_ncol), aes(value)) +
 dev.off()
 system('pdflatex v10acs_U_post_ncol.tex')
 
+# Model 2
 prior_df_col_U <- data.frame(value = c(seq(min(df_U_col$N), max(df_U_col$N), length= 100), 
                                        seq(min(df_U_col$R), max(df_U_col$R), length= 100),
                                        seq(min(df_U_col$n), max(df_U_col$n), length= 100),
@@ -229,7 +240,7 @@ ggplot(gather(df_U_col), aes(value)) +
 dev.off()
 system('pdflatex v10acs_U_post_col.tex')
 
-# density and probability plots
+# density and probability plots for X_c
 ncol_dat <- res[[1]][-c(1:10000)]
 col_dat <- res_col[[1]][-c(1:10000)]
 
@@ -243,6 +254,7 @@ X_c <- as.data.frame(X_c)
 
 U <- data.frame(x = 3609, y = 631)/4300
 
+# Performance metrics comparison between Model 1 and Model 2
 ci <- c(0.1, 0.2, 0.3, 0.4)
 dm <- sapply(ci, function(x) detect_metric(U, 0.03, x, X_nc, X_c))
 acc_bs <- vector('list', length = 4)
@@ -271,24 +283,9 @@ for(i in 1:4){
 
 saveRDS(list(dm, acc_bs, FP_bs, prec_bs), 'data/v10acs_res/v10acs_pm.RDS')
 
-dat_dm <- data.frame(prob = 1 - ci, acc = unlist(dm[1,]), FP = unlist(dm[2,]), prec = unlist(dm[3,]), 
-                     TP_upper = unlist(lapply(acc_bs, function(x) quantile(x, 0.975))), 
-                     TP_lower = unlist(lapply(acc_bs, function(x) quantile(x, 0.025))),
-                     FP_upper = unlist(lapply(FP_bs, function(x) quantile(x, 0.975))), 
-                     FP_lower = unlist(lapply(FP_bs, function(x) quantile(x, 0.025))),
-                     prec_upper = unlist(lapply(prec_bs, function(x) quantile(x, 0.975))),
-                     prec_lower = unlist(lapply(prec_bs, function(x) quantile(x, 0.025))))
-
+# 2d Density plots of X_c under Model 1 and Model 2 
 options(tikzLatexPackages 
         =c(getOption( "tikzLatexPackages" ),"\\usepackage{amsfonts}"))
-
-tikz(file = "v10acs_prec.tex", standAlone=T, width = 3.5, height = 3.5)
-ggplot(dat_dm, aes(prob, prec)) + geom_line(size = 0.2, color = "#A25050") + 
-  geom_pointrange(size = 0.2, aes(ymin = prec_lower, ymax = prec_upper), color = "#7C0000") + 
-  ylim(c(0.8,1.6)) + geom_abline(intercept = 1, slope = 0, linetype = 'dashed', size = 0.2) + theme_minimal() +
-  xlab('$1-\\alpha$') + ylab('Prec(Model 1)/Prec(Model 2)')
-dev.off()
-system('pdflatex v10acs_prec.tex')
 
 tikz(file = "v10acs_model1_res.tex", standAlone=T, width = 5, height = 4.5)
 ggplot(C_post_r, aes(x, y)) +  stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE, n = 200) + 
